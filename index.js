0f9c63ad6ad808b6e5ddf7d2c8fc64935b5374c0
Christina Burchette <burchc426@gmail.com>
2018-12-18 01:10:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = { }
 
 function updateDriverWithKeyAndValue(driver,name,address) {
-  const newDriver =
+  const newDriver = {...driver}
 }
 
 function destructivelyUpdateDriverWithKeyAndValue () {

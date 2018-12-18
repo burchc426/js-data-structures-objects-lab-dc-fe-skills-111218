ea9189912bbffed7fdfd5ee3e5454130fceab697
Christina Burchette <burchc426@gmail.com>
2018-12-18 01:34:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ function destructivelyUpdateDriverWithKeyAndValue (driver,key,value) {
 }
 
 function deleteFromDriverByKey () {
-  
+  const obj = { foo: "bar" };
+ 
+const newObj = Object.assign({}, obj);
+ 
+newObj;
+// => { foo: "bar" }
+ 
+delete newObj.foo;
+// => true
+ 
 }
 
 function destructivelyDeleteFromDriverByKey () {
